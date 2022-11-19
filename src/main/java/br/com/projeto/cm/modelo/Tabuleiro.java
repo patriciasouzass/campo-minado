7fec1ffe4a66a5860e1d8ee14854910ba25e6950
Patricia Souza <patricia.asouza@mercadolivre.com>
2022-11-18 21:41:39 -0300

    added new methods and adjustments

--- src/main/java/br/com/projeto/cm/modelo/Tabuleiro.java
+++ src/main/java/br/com/projeto/cm/modelo/Tabuleiro.java
@@ -1,4 +1,90 @@
 package br.com.projeto.cm.modelo;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
 public class Tabuleiro {
+
+    private int qtdLinhas;
+    private int qtdColunas;
+    private int qtdMinas;
+
+    private final List<Campo> campos = new ArrayList();
+
+    public Tabuleiro(int qtdLinhas, int qtdColunas, int qtdMinas) {
+        this.qtdLinhas = qtdLinhas;
+        this.qtdColunas = qtdColunas;
+        this.qtdMinas = qtdMinas;
+
+        gerarCampos();
+        associarVizinhos();
+        sortearMinas();
+    }
+
+    public void abrir(int linha, int coluna) {
+        campos.parallelStream()
+                .filter(c -> c.getLinha() == linha && c.getColuna() == coluna)
+                .findFirst()
+                .ifPresent(c -> c.abrir());
+    }
+
+    public void alternarMarcacao(int linha, int coluna) {
+        campos.parallelStream()
+                .filter(c -> c.getLinha() == linha && c.getColuna() == coluna)
+                .findFirst()
+                .ifPresent(c -> c.alternarMarcacao());
+    }
+
+    private void gerarCampos() {
+        for (int linha = 0; linha < qtdLinhas; linha++) {
+            for (int coluna = 0; coluna < qtdColunas; coluna++) {
+                campos.add(new Campo(linha, coluna));
+            }
+        }
+    }
+
+    private void associarVizinhos() {
+        for (Campo c1 : campos) {
+            for (Campo c2 : campos) {
+                c1.adicionarVizinho(c2);
+            }
+        }
+    }
+
+    private void sortearMinas() {
+        long minasArmadas = 0;
+        Predicate<Campo> minado = c -> c.isMinado();
+
+        do {
+            int aleatorio = (int) (Math.random() * campos.size());
+            campos.get(aleatorio).minar();
+            minasArmadas = campos.stream().filter(minado).count();
+        } while (minasArmadas < qtdMinas);
+    }
+
+    public boolean objetivoAlcancado() {
+        return campos.stream().allMatch(c -> c.objetivoAlcancado());
+    }
+
+    public void reiniciar() {
+        campos.stream().forEach(c -> c.reiniciar());
+        sortearMinas();
+    }
+
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+
+        int i = 0;
+        for (int l = 0; l < qtdLinhas; l++) {
+            for (int c = 0; c < qtdColunas; c++) {
+                sb.append(" ");
+                sb.append(campos.get(i));
+                sb.append(" ");
+                i++;
+            }
+            sb.append("\n");
+        }
+        return sb.toString();
+    }
 }
