6184a36bb253d31926cd9ea008b7ae31323a534f
Patricia Souza <patricia.asouza@mercadolivre.com>
2022-11-18 23:40:46 -0300

    adjustments

--- src/main/java/br/com/projeto/cm/Aplicacao.java
+++ src/main/java/br/com/projeto/cm/Aplicacao.java
@@ -1,6 +1,7 @@
 package br.com.projeto.cm;
 
 import br.com.projeto.cm.modelo.Tabuleiro;
+import br.com.projeto.cm.visao.TabuleiroConsole;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,10 +11,12 @@ public class Aplicacao {
         SpringApplication.run(Aplicacao.class, args);
         Tabuleiro tabuleiro = new Tabuleiro(6, 6, 6);
 
-        tabuleiro.alternarMarcacao(4, 4);
-        tabuleiro.alternarMarcacao(4, 5);
-        tabuleiro.abrir(3, 3);
+        new TabuleiroConsole(tabuleiro);
 
-        System.out.println(tabuleiro);
+//        tabuleiro.alternarMarcacao(4, 4);
+//        tabuleiro.alternarMarcacao(4, 5);
+//        tabuleiro.abrir(3, 3);
+//
+//        System.out.println(tabuleiro);
     }
 }
